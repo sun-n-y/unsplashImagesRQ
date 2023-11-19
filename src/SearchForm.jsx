5af14144be5218a76dca6c9e841e6ff47bbdc3b8
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-19 15:04:36 -0500

    add search form logic

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,4 +1,27 @@
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className="title">unsplash</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
+
 export default SearchForm;
