c8cb94950e3b8be99ca57e7eb6a5cb56b17be0bd
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-19 15:50:51 -0500

    add get url logic

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const url = 'https://api.unsplash.com/search/photos/?query=cat&client_id=';
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
+
+  console.log(response);
+
   return <h2>Gallery</h2>;
 };
 export default Gallery;
