03bee9cd06446a9940ec9dc5e1c775cf04dd88ca
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-20 12:21:59 -0500

    add env variable

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,8 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useGlobalContext } from './context';
 
-const url =
-  'https://api.unsplash.com/search/photos/?client_id=zQeSH7nbhsoaAAlExPVJ7KNVWVvVhgCZKtSwsS2x0UA';
+const url = `https://api.unsplash.com/search/photos/?client_id=${
+  import.meta.env.VITE_API_KEY
+}`;
 
 const Gallery = () => {
   const { searchTerm } = useGlobalContext();
